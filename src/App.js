b35c4ebe506583af813f9f86059458f82d87ede4
WILSON PAREDES <wilsonparedes87@gmail.com>
2022-07-06 20:50:42 -0500

    Desarrollo del About

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Cover from './components/Cover';
 import Navbar from './components/Navbar';
-
+import About from './components/About';
 function App() {
 
   const [scrollHeight, setScrollHeight] = useState(0);
 
-  const handleScroll = () =>{
+  const handleScroll = () => {
     const position = window.pageYOffset;
     setScrollHeight(position)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-  },[scrollHeight]);
+  }, [scrollHeight]);
 
   return (
     <div className="App">
-      <Navbar isScrolling = {scrollHeight} />
+      <Navbar isScrolling={scrollHeight} />
       <Cover />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/About.css"
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="about-desc">
+                <h3>Dejame contarte algo sobre mi</h3>
+                <p>Soy un estudiante de ingeniería de software apasionado por la tecnología </p>
+            </div>
+            <div className="about-img">
+                <img src="https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg"
+                    alt="about"/>
+            </div>
+        </div>
+
+   );
+};
+
+export default About;
